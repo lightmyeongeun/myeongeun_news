e9ea06c636e09e7e219ce0f88efe2cc4ebbd58c5
lightmyeongeun <cme202908@naver.com>
2024-02-21 01:07:43 +0900

    day01

---
+++ main.js
@@ -0,0 +1,13 @@
+const API_KEY=[`220f91ed65214e4c99148f77e7370d6c`]
+let news=[]
+
+const getLatesNews=async()=>{
+    const url=new URL(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+        const response = await fetch(url)
+        const data=await response.json()
+        news=data.articles
+        console.log("ddd",news)
+}
+
+getLatesNews()
