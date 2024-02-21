7373c049d4feca8cb643a371fba333cfb1224949
lightmyeongeun <cme202908@naver.com>
2024-02-22 02:22:38 +0900

    wednesday

--- main.js
+++ main.js
@@ -13,38 +13,43 @@ const getLatestNews=async()=>{
         console.log("ddd",newsList)
 }
 
-getLatestNews()
+getLatestNews()//api 불러오기
 
 
 const render=()=>{
     const newsHTML=newsList.map(news=>`<div class="row news">
     <div class="col-lg-4">
-        <img class="news_img_size" src="${news.urlToImage}">
+        <img class="news_img_size" src="${news.urlToImage || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+    }" />
     </div>
     <div class="col-lg-8">
         <h2>${news.title}</h2>
-        <p>${news.description}</p>
-        <div>${news.source.name} * ${news.publishedAt}</div>
+        <p>${news.description==null || news.description==""
+        ? "내용없음"
+        : news.description.length > 200
+        ? news.description.substring(0,200)+"..."
+        : news.description}</p>
+        <div>${news.source.name || "no source"} ${moment(news.publishedAt).startOf('day').fromNow()}</div>
     </div>
 </div>`).join("")
     document.getElementById("news_board").innerHTML=newsHTML
-}
+} 
+// UI에 뉴스 불러오기
+//news.description 삼항연산식 *
+//img 추가 *
+//cdn으로 시간 설정하기! moment().startOf('day'). fromNow()
+let input = document.querySelector(".search_area");
+let btn = document.querySelector(".search_btn");
+let parent = document.querySelector(".search");
 
-//api키를 통해서 ul보여주기
+btn.addEventListener("click", () => {
+    parent.classList.toggle("active");
+});//검색창 띄우기
 
-// function render(){
-//     let resultHTML=""
-//     let list=[]
-//     for(let i=0;i<list.length;i++){
-//         resultHTML+=`<div class="row news">
-//         <div class="col-lg-4">
-//             <img class="news_img_size" src=${list[i].taskContent} alt="">
-//         </div>
-//         <div class="col-lg-8">
-//             <h2>${list[i]taskTittle}</h2>
-//             <p>코딩알려주는 누나가 자바스크립트 강의를 오픈했다. 강의가 완판되고 팬미팅도 하게 되었다</p>
-//             <div>KBS * 24.02.21</div>
-//         </div>
-//     </div>`
-//     }
-// }
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "250px";
+}; 
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+}; // 반응형 사이드 - 복습 필요!!*
+
