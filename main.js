7d6775464b8080575b8dfd624a88ab81cbedb577
lightmyeongeun <cme202908@naver.com>
2024-02-23 17:22:23 +0900

    firday commit

--- main.js
+++ main.js
@@ -4,10 +4,38 @@ let url=new URL(`https://myeongeunnews.netlify.app/top-headlines?country=kr`)
 // `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
 
 const newsData=async()=>{
-    const response = await fetch(url)
-    const data=await response.json()
-    newsList=data.articles
-    render()
+    try{
+        const response = await fetch(url)
+        const data=await response.json()
+        if(response.status==200){//api 값이 200(정상)이 아닐때 에러메세지
+            if(data.articles.length===0){
+                throw new Error("No result for this search")
+            }//검색어 일치 아닐때 에러메세지 => 이게 아니면 다시 흘러감
+            newsList=data.articles
+            render()
+        }else{
+            throw new Error(data,message)
+        }
+    }catch(error){
+        errorRender(error.message)
+    }
+}
+
+const errorRender=(errorMessage)=>{
+    const errorHTML=`<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+    <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+    </symbol>
+  </svg>
+    <div class="alert alert-danger d-flex align-items-center" role="alert">
+    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+        <use xlink:href="#exclamation-triangle-fill"/>
+    </svg>
+    <div>
+    ${errorMessage}
+    </div>
+  </div>`    
+    document.getElementById("news_board").innerHTML=errorHTML
 }
 
 const getLatestNews=async()=>{
@@ -34,6 +62,8 @@ const render=()=>{
 </div>`).join("")
     document.getElementById("news_board").innerHTML=newsHTML
 } 
+
+
 
 // UI에 뉴스 불러오기
 //news.description 삼항연산식 *
@@ -83,8 +113,9 @@ const searchKeywords=async()=>{
     newsData()
 }
 
+//키워드 - enter키
 input.addEventListener("keydown", function (event) {
     if (event.keyCode === 13) {
       searchKeywords(event);
     }
-});//enter키
+});
